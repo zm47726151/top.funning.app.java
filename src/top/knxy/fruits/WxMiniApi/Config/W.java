18c49139f14ad9b117bfcecd8e3cdee4a2d5bcf2
MrNeptune <45159862+MrNeptune@users.noreply.github.com>
2019-03-01 15:47:29 +0800

    Delete W.java

--- src/top/knxy/fruits/WxMiniApi/Config/W.java
+++
@@ -1,7 +0,0 @@
-package top.knxy.fruits.WxMiniApi.Config;
-
-public class W {
-    public static String appid = "wxe1f28f4b32adfad1";
-    public static String secret = "a6ccc9d652f9a9f57ab273e6a3139db8";
-
-}
