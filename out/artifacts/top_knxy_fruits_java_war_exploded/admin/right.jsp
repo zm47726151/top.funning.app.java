0dbaa6b451aa9ec62932b4b9cb8b52ce08427830
MrNeptune <ywj-1991@qq.com>
2019-03-01 20:29:15 +0800

    c

---
+++ out/artifacts/top_knxy_fruits_java_war_exploded/admin/right.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: faddenyin
+  Date: 2019-03-01
+  Time: 19:46
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+
+</body>
+</html>
--- src/top/knxy/fruits/Servlet/admin/Index.java
+++ src/top/knxy/fruits/Servlet/admin/Index.java
@@ -7,13 +7,18 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "admin_index", urlPatterns = "/admin/index")
+@WebServlet(name = "admin_index", urlPatterns = {"/admin/index", "/admin/order"})
 public class Index extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.getRequestDispatcher("/admin/index.jsp").forward(request, response);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String requestURI = request.getRequestURI();
+        int index = requestURI.lastIndexOf("/");
+        String page = requestURI.substring(index);
+        request.setAttribute("jspPage", page + ".jsp");
+        request.setAttribute("page", page);
         request.getRequestDispatcher("/admin/index.jsp").forward(request, response);
     }
 }
