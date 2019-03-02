8b1a59ad2f1d2ef695f76fb29fad806b42a0b65f
MrNeptune <ywj-1991@qq.com>
2019-03-02 10:43:34 +0800

    init

---
+++ src/top/knxy/fruits/Servlet/admin/Order/Search.java
@@ -0,0 +1,9 @@
+package top.knxy.fruits.Servlet.admin.Order;
+
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(urlPatterns = {"/admin/order/search"})
+public class Search extends HttpServlet {
+}
