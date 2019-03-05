87b0d18b82791981e74a19229364cf15aa2f865b
MrNeptune <ywj-1991@qq.com>
2019-03-05 11:02:21 +0800

    c

---
+++ out/artifacts/top_knxy_fruits_java_war_exploded/js/common.js
@@ -0,0 +1,41 @@
+let Web = {
+    request: function (cmd, data, listenet) {
+        if (cmd == null) {
+            alert("local error:cmd is empty")
+            return;
+        }
+        let json = {
+            type: "POST",
+            url: "/admin/api",
+            dataType: "Json",
+            success: function (response) {
+                console.log(response);
+                if (response.code == "1") {
+                    listenet.onSuccess(response);
+                } else {
+                    listenet.onError(response);
+                }
+            },
+            error: function (xmlHttpRequest, errorMsg, exception) {
+                console.log(response);
+                let response = {
+                    code: "L0000",
+                    msg: errorMsg
+                };
+                listenet.onError(response);
+            }
+        }
+
+        let postJson = {
+            "cmd": cmd
+        };
+        if (data != null) {
+            postJson.data = data;
+        }
+        json.data = JSON.stringify({
+            "cmd": cmd,
+            "data": data
+        });
+        $.ajax(json);
+    }
+}
