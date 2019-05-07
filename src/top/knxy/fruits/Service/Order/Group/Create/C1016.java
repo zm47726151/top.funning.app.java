0dac3a69d64e197ec384863f03bf872edbb80444
MrNeptune <ywj-1991@qq.com>
2019-05-07 15:25:09 +0800

    拼团开发

---
+++ src/top/knxy/fruits/Service/Order/Group/Create/C1016.java
@@ -0,0 +1,87 @@
+package top.knxy.fruits.Service.Order.Group.Create;
+
+import org.apache.ibatis.session.SqlSession;
+import top.knxy.fruits.DataBase.DAL.GroupGoodDAL;
+import top.knxy.fruits.DataBase.DAL.GroupOrderDAL;
+import top.knxy.fruits.DataBase.Table.GroupGood;
+import top.knxy.fruits.DataBase.Table.GroupOrder;
+import top.knxy.library.BaseService;
+import top.knxy.library.ServiceException;
+import top.knxy.library.Utils.MyBatisUtils;
+import top.knxy.library.Utils.ServiceUtils;
+import top.knxy.library.Utils.TextUtils;
+
+import java.util.Date;
+
+public class C1016 extends BaseService {
+    public String id;
+    public String teamId;
+    public String userId;
+
+    @Override
+    protected void run() throws Exception {
+        if (TextUtils.isEmpty(id)) {
+            throw new ServiceException("id is empty");
+        }
+
+        SqlSession session = MyBatisUtils.getSession();
+        GroupGoodDAL ggDal = session.getMapper(GroupGoodDAL.class);
+        GroupOrderDAL goDal = session.getMapper(GroupOrderDAL.class);
+        GroupGood model = ggDal.get(id);
+        if (model == null) {
+            session.close();
+            throw new ServiceException("not such GroupGood, id = " + id + " , userId = " + userId);
+        }
+
+        if (!TextUtils.isEmpty(teamId)) {
+            if (1 > goDal.exist(id, teamId)) {
+                session.close();
+                throw new ServiceException("not exist teamId, id = " + id +
+                        " , userId = " + userId +
+                        " , teamId = " + teamId);
+            }
+        } else {
+            teamId = ServiceUtils.getUUid();
+        }
+
+        GroupOrder go = new GroupOrder();
+        go.setId(ServiceUtils.getUUid());
+        go.setPrice(model.getPrice());
+        go.setGetTimeStart(model.getGetTimeStart());
+        go.setGetTimeStop(model.getGetTimeStop());
+        go.setGroupNum(model.getGroupNum());
+        go.setUserId(userId);
+        go.setCreateDT(new Date());
+        go.setGroupGoodId(model.getId());
+        go.setName(model.getName());
+        go.setDescription(model.getDescription());
+        go.setImageUrl(model.getImageUrl());
+        go.setState(1);
+        go.setTeamId(teamId);
+
+        if (goDal.create(go) < 1) {
+            session.close();
+            throw new ServiceException("create order failure");
+        }
+        session.commit();
+
+        Data data = new Data();
+        data.id = go.getId();
+        this.data = data;
+
+        session.close();
+        ServiceUtils.createSuccess(this);
+    }
+
+    public static class Data {
+        public String id;
+    }
+
+    public static class Good {
+        public String id;
+        public String name;
+        public String description;
+        public String imageUrl;
+        public String price;
+    }
+}
---
+++ src/top/knxy/fruits/Service/Order/Group/Get/C1017.java
@@ -0,0 +1,161 @@
+package top.knxy.fruits.Service.Order.Group.Get;
+
+import org.apache.ibatis.session.SqlSession;
+import top.knxy.fruits.DataBase.DAL.GroupOrderDAL;
+import top.knxy.fruits.DataBase.Table.GroupOrder;
+import top.knxy.library.BaseService;
+import top.knxy.library.Utils.MyBatisUtils;
+import top.knxy.library.Utils.ServiceUtils;
+import top.knxy.library.Utils.TextUtils;
+
+import java.util.Date;
+
+public class C1017 extends BaseService {
+    public String id;
+    public String userId;
+
+    @Override
+    protected void run() throws Exception {
+        if (TextUtils.isEmpty(id) || TextUtils.isEmpty(userId)) {
+            throw new NullPointerException();
+        }
+
+        SqlSession session = MyBatisUtils.getSession();
+        GroupOrderDAL dal = session.getMapper(GroupOrderDAL.class);
+        GroupOrder go = dal.get(id, userId);
+        this.data = go;
+
+        session.close();
+        ServiceUtils.createSuccess(this);
+    }
+
+
+    public static class GroupOrder {
+        private String id;
+        private String price;
+        private Date getTimeStart;
+        private Date getTimeStop;
+        private String groupNum;
+        private String groupGoodId;
+        private String name;
+        private String description;
+        private String imageUrl;
+        private String state;
+        private String stateStr;
+        private Date createDT;
+        private Date payDT;
+
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public String getPrice() {
+            return price;
+        }
+
+        public void setPrice(String price) {
+            this.price = price;
+        }
+
+        public Date getGetTimeStart() {
+            return getTimeStart;
+        }
+
+        public void setGetTimeStart(Date getTimeStart) {
+            this.getTimeStart = getTimeStart;
+        }
+
+        public Date getGetTimeStop() {
+            return getTimeStop;
+        }
+
+        public void setGetTimeStop(Date getTimeStop) {
+            this.getTimeStop = getTimeStop;
+        }
+
+        public String getGroupNum() {
+            return groupNum;
+        }
+
+        public void setGroupNum(String groupNum) {
+            this.groupNum = groupNum;
+        }
+
+        public String getGroupGoodId() {
+            return groupGoodId;
+        }
+
+        public void setGroupGoodId(String groupGoodId) {
+            this.groupGoodId = groupGoodId;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
+
+        public String getImageUrl() {
+            return imageUrl;
+        }
+
+        public void setImageUrl(String imageUrl) {
+            this.imageUrl = imageUrl;
+        }
+
+        public String getState() {
+            return state;
+        }
+
+        public void setState(String state) {
+            this.state = state;
+
+            if ("1".equals(state)) {
+                this.stateStr = "待付款";
+            } else if ("2".equals(state)) {
+                this.stateStr = "拼团中";
+            } else if ("3".equals(state)) {
+                this.stateStr = "待取货";
+            } else if ("4".equals(state)) {
+                this.stateStr = "已完成";
+            } else if ("5".equals(state)) {
+                this.stateStr = "退款中";
+            } else if ("6".equals(state)) {
+                this.stateStr = "已取消";
+            } else if ("7".equals(state)) {
+                this.stateStr = "已退款";
+            }
+        }
+
+        public Date getCreateDT() {
+            return createDT;
+        }
+
+        public void setCreateDT(Date createDT) {
+            this.createDT = createDT;
+        }
+
+        public Date getPayDT() {
+            return payDT;
+        }
+
+        public void setPayDT(Date payDT) {
+            this.payDT = payDT;
+        }
+
+    }
+}
