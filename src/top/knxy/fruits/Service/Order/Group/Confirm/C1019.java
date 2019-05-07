efbc0b9541e7b9e25eec2bd261b3f9ab67076c8f
MrNeptune <ywj-1991@qq.com>
2019-05-07 15:25:09 +0800

    拼团开发

---
+++ src/top/knxy/fruits/Service/Order/Group/Confirm/C1019.java
@@ -0,0 +1,13 @@
+package top.knxy.fruits.Service.Order.Group.Confirm;
+
+import top.knxy.library.BaseService;
+
+public class C1019 extends BaseService {
+    public String userId;
+    public String id;
+
+    @Override
+    protected void run() throws Exception {
+
+    }
+}
