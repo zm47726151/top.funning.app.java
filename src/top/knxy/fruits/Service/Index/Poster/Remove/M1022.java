669055c514107332603f91358991ccbdc8eaf1cb
MrNeptune <ywj-1991@qq.com>
2019-05-07 15:18:57 +0800

    redis use

--- src/top/knxy/fruits/Service/Index/Poster/Remove/M1022.java
+++ src/top/knxy/fruits/Service/Index/Poster/Remove/M1022.java
@@ -1,7 +1,7 @@
 package top.knxy.fruits.Service.Index.Poster.Remove;
 
+import top.knxy.fruits.DataBase.Redis;
 import top.knxy.library.Config.V;
-import top.knxy.fruits.DataBase.Cache.AppMap;
 import top.knxy.library.BaseService;
 import top.knxy.library.Utils.ServiceUtils;
 
@@ -10,7 +10,7 @@ public class M1022 extends BaseService {
     @Override
     public void run() throws Exception {
 
-        AppMap.remove(V.postImageUrl);
+        Redis.del(V.postImageUrl);
         ServiceUtils.createSuccess(this);
     }
 
